7e5b51834a3d2a734d355eb3caa90dd1a862f509
Rahul V.B <r-vb@rahulvb.com>
2024-02-19 14:06:24 +0530

    19/2 14.06

---
+++ basics/saving-files/bill.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// amke new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+
+	return b
+}
+
+// format the bill
+func (b *bill) format() string {
+	fs := "\nBill Breakdown: \n"
+	var total float64 = 0
+
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...₹%v \n", k+":", v)
+		total += v
+	}
+
+	// add tip
+	fs += fmt.Sprintf("%-25v ...₹%0.2f \n", "tip:", b.tip)
+
+	// total
+	fs += fmt.Sprintf("%-25v ...₹%0.2f \n", "total:", total+b.tip)
+
+	return fs
+}
+
+// update the tip
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip // or (*b).tip = tip --> de-referencing
+}
+
+// add item to the bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+// save bill
+func (b *bill) save() {
+	date := []byte(b.format())
+
+	err := os.WriteFile("bills/"+b.name+".txt", date, 0644)
+	if  err != nil {
+		panic(err)
+	}
+	fmt.Println("Bill was saved to file!")
+}
